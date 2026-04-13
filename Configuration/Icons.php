cc12e7abbbcc8758e25fdd2dbc07d4e5cb0fb308
Sven Wappler <wappler@wappler.systems>
2026-04-13 22:05:10 +0200

    Refactor method signatures to include return types in finishers

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'plugin-feregistration' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:fe_registration/Resources/Public/Icons/PluginDoubleOptIn.svg',
+    ],
+];
